8ae826d97e17f4b5238a213d87f52e7aa6d601e8
Albert <kibernetik.b02@mail.ru>
2023-05-20 00:45:42 +0300

    Временно отклчючены разрешенные источники CORS

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ def get_app() -> FastAPI:
     app.add_event_handler('shutdown', events.on_shutdown)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins='https://gefest.tech',
+        allow_origins='*',
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"]

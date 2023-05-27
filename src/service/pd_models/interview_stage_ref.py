c060633c402a8dc2dfb181b1fc2d29c4b47b66c6
Albert <kibernetik.b02@mail.ru>
2023-05-27 08:51:46 +0300

    Исправлены типы полей в моделях некоторых справочников

--- src/service/pd_models/interview_stage_ref.py
+++ src/service/pd_models/interview_stage_ref.py
@@ -1,10 +1,11 @@
+import typing as tp
 from pydantic import BaseModel
 
 
 class Read(BaseModel):
     code: int
     value: str
-    parent_id: int
+    parent_id: tp.Optional[int]
     
     class Config:
         orm_mode = True
